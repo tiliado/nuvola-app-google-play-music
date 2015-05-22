ebe1744e9276e524b6cbf668a4e36a92c36ec75d
Jiří Janoušek <janousek.jiri@gmail.com>
2015-05-22 19:11:15 +0200

    Port to new design
    * Fixed playback details and actions
    * Removed star rating actions (gone)
    
    Issue: tiliado/nuvolaplayer#100
    
    Signed-off-by: Jiří Janoušek <janousek.jiri@gmail.com>

--- integrate.js
+++ integrate.js
@@ -35,11 +35,7 @@ var player = Nuvola.$object(Nuvola.MediaPlayer);
 
 var ACTION_THUMBS_UP = "thumbs-up";
 var ACTION_THUMBS_DOWN = "thumbs-down";
-var ACTION_RATING = "rating";
 var THUMBS_ACTIONS = [ACTION_THUMBS_UP, ACTION_THUMBS_DOWN];
-var STARS_ACTIONS = [];
-for (var i=0; i <= 5; i++)
-    STARS_ACTIONS.push(ACTION_RATING + "::" + i);
 
 var WebApp = Nuvola.$WebApp();
 
@@ -49,17 +45,6 @@ WebApp._onInitAppRunner = function(emitter)
     
     Nuvola.actions.addAction("playback", "win", ACTION_THUMBS_UP, C_("Action", "Thumbs up"), null, null, null, true);
     Nuvola.actions.addAction("playback", "win", ACTION_THUMBS_DOWN,C_("Action", "Thumbs down"), null, null, null, true);
-    var ratingOptions = [];
-    for (var stars = 0; stars < 6; stars++)
-        ratingOptions.push([
-            stars, // stateId, label, mnemo_label, icon, keybinding
-            /// Star rating, {1} is a placeholder for a number 
-            Nuvola.format(ngettext("Rating: {1} star", "Rating: {1} stars", stars), stars), // label
-            null, // mnemo_label
-            null, // icon
-            null  // keybinding
-            ]);
-    Nuvola.actions.addRadioAction("playback", "win", ACTION_RATING, 0, ratingOptions);
 }
 
 WebApp._onInitWebWorker = function(emitter)
@@ -69,10 +54,8 @@ WebApp._onInitWebWorker = function(emitter)
     Nuvola.actions.connect("ActionActivated", this);
     this.thumbsUp = undefined;
     this.thumbsDown = undefined;
-    this.starRating = undefined;
-    this.starRatingEnabled = undefined;
-    this.thumbRatingEnabled = undefined;
     this.state = State.UNKNOWN;
+    player.addExtraActions(THUMBS_ACTIONS);
     document.addEventListener("DOMContentLoaded", this._onPageReady.bind(this));
 }
 
@@ -93,7 +76,6 @@ WebApp._onUriChanged = function(emitter, uri)
 
 WebApp._onPageReady = function()
 {
-    this.addNavigationButtons();
     this.update();
 }
 
@@ -102,7 +84,7 @@ WebApp.update = function()
     var track = {};
     try
     {
-        track.artLocation = document.getElementById('playingAlbumArt').src;
+        track.artLocation = document.getElementById('playingAlbumArt').src.replace("=s90-", "=s500-");
     }
     catch(e)
     {
@@ -111,7 +93,7 @@ WebApp.update = function()
     
     try
     {
-        var elm = document.getElementById('playerSongTitle').firstChild;
+        var elm = document.getElementById('player-song-title').firstChild;
         track.title = elm.innerText || elm.textContent;
     }
     catch(e)
@@ -146,18 +128,18 @@ WebApp.update = function()
     try
     {
         var buttons = document.querySelector("#player .player-middle");
-        var pp = buttons.childNodes[2];
+        var pp = this._getPlayPauseButton();
         if (pp.disabled === true)
             this.state = State.UNKNOWN;
-        else if (pp.className == "flat-button playing")
+        else if (pp.className == "playing")
             this.state = State.PLAYING;
         else
             this.state = State.PAUSED;
         
         if (this.state !== State.UNKNOWN)
         {
-            prevSong = buttons.childNodes[1].disabled === false;
-            nextSong = buttons.childNodes[3].disabled === false;
+            prevSong = this._getGoPrevButton().disabled === false;
+            nextSong = this._getGoNextButton().disabled === false;
         }
         else
         {
@@ -178,37 +160,12 @@ WebApp.update = function()
     // Extract enabled flag and state from a web page
     var actionsEnabled = {};
     var actionsStates = {};
-    try
-    {
-        actionsEnabled[ACTION_THUMBS_UP] = false;
-        actionsEnabled[ACTION_THUMBS_DOWN] = false;
-        var thumbs = this.getThumbs();
-        if (thumbs[0].style.visibility !== "hidden")
-        {
-            this.toggleThumbRating(true);
-            actionsStates[ACTION_THUMBS_UP] = thumbs[1].className === "selected";
-            actionsStates[ACTION_THUMBS_DOWN] = thumbs[2].className === "selected";
-            actionsEnabled[ACTION_THUMBS_UP] = true;
-            actionsEnabled[ACTION_THUMBS_DOWN] = true;
-        }
-    }
-    catch (e)
-    {
-    }
-    try
-    {
-        actionsEnabled[ACTION_RATING] = false;
-        var stars = this.getStars();
-        if (stars.style.visibility !== "hidden")
-        {
-            this.toggleStarRating(true);
-            actionsStates[ACTION_RATING] = stars.childNodes[0].getAttribute("data-rating") * 1;
-            actionsEnabled[ACTION_RATING] = true;
-        }
-    }
-    catch (e)
-    {
-    }
+    var thumbsUp = this._getThumbsUpButton();
+    var thumbsDown = this._getThumbsDownButton();
+    actionsStates[ACTION_THUMBS_UP] = this._isThumbSelected(thumbsUp);
+    actionsStates[ACTION_THUMBS_DOWN] = this._isThumbSelected(thumbsDown);
+    actionsEnabled[ACTION_THUMBS_UP] = !!thumbsUp;
+    actionsEnabled[ACTION_THUMBS_DOWN] = !!thumbsDown;
     
     // Compare with previous values and update if necessary
     Nuvola.actions.updateEnabledFlags(actionsEnabled);
@@ -217,154 +174,96 @@ WebApp.update = function()
     setTimeout(this.update.bind(this), 500);
 }
 
-WebApp.getPlayerButtons = function()
+
+WebApp._getButton = function(id)
 {
-    var elm = document.querySelector("#player .player-middle");
-    return elm ? elm.childNodes : null;
+    return document.querySelector("sj-icon-button[data-id=" + id + "]");
+}
+
+WebApp._getPlayPauseButton = function()
+{
+    return this._getButton("play-pause");
+}
+
+WebApp._getGoPrevButton = function()
+{
+    return this._getButton("rewind");
+}
+
+WebApp._getGoNextButton = function()
+{
+    return this._getButton("forward");
+}
+
+WebApp._getShuffleButton = function()
+{
+    return this._getButton("shuffle");
+}
+
+WebApp._getThumbsUpButton = function()
+{
+    return document.querySelector(".player-rating-container sj-icon-button[data-rating='5']");
+}
+
+WebApp._getThumbsDownButton = function()
+{
+    return document.querySelector(".player-rating-container sj-icon-button[data-rating='1']");
+}
+
+WebApp._isThumbSelected = function(elm)
+{
+    return elm ? elm.$.icon.getAttribute('aria-label').indexOf('-outline') == -1: false;
 }
 
 WebApp._onActionActivated = function(emitter, name, param)
 {
-    var buttons = this.getPlayerButtons();
-    if (buttons)
-    {
-        var prev_song = buttons[1];
-        var next_song = buttons[3];
-        var play_pause = buttons[2];
-    }
-    else
-    {
-        var prev_song = null;
-        var next_song = null;
-        var play_pause = null;
-    }
+    var prevSong = this._getGoPrevButton();
+    var nextSong = this._getGoNextButton();
+    var playPause = this._getPlayPauseButton();
+    var luckyMix = this._luckyMix();
     
     switch (name)
     {
     /* Base media player actions */
     case PlayerAction.TOGGLE_PLAY:
-        var luckyMix = this._luckyMix();
         if (this.state === State.UNKNOWN && luckyMix)
-            Nuvola.clickOnElement(luckyMix);
+            luckyMix.click();
         else
-            Nuvola.clickOnElement(play_pause);
+            playPause.click();
         break;
     case PlayerAction.PLAY:
-        var luckyMix = this._luckyMix();
         if (this.state === State.UNKNOWN && luckyMix)
-            Nuvola.clickOnElement(luckyMix);
+            luckyMix.click();
         else if (this.state != State.PLAYING)
-            Nuvola.clickOnElement(play_pause);
+            playPause.click();
         break;
     case PlayerAction.PAUSE:
     case PlayerAction.STOP:
         if (this.state == State.PLAYING)
-            Nuvola.clickOnElement(play_pause);
+            playPause.click();
         break;
     case PlayerAction.PREV_SONG:
-        if (prev_song)
-            Nuvola.clickOnElement(prev_song);
+        if (prevSong)
+           prevSong.click();
         break;
     case PlayerAction.NEXT_SONG:
-        if (next_song)
-            Nuvola.clickOnElement(next_song);
+        if (nextSong)
+            nextSong.click();
         break;
     
     /* Custom actions */
     case ACTION_THUMBS_UP:
-        Nuvola.clickOnElement(this.getThumbs()[1]);
+        this._getThumbsUpButton().click();
         break;
     case ACTION_THUMBS_DOWN:
-        Nuvola.clickOnElement(this.getThumbs()[2]);
+        this._getThumbsDownButton().click();
         break;
-    case ACTION_RATING:
-        var stars = this.getStars().childNodes;
-        var i = stars.length;
-        while (i--)
-        {
-            var star = stars[i];
-            if (star.getAttribute("data-rating") === ("" + param))
-            {
-                Nuvola.clickOnElement(star);
-                break;
-            }
-        }
-        break;
-    }
-}
-
-WebApp.addNavigationButtons = function()
-{
-    /* Loading in progress? */
-    var loading = document.getElementById("loading-progress");
-    if (loading && loading.style.display != "none")
-    {
-        setTimeout(this.addNavigationButtons.bind(this), 250);
-        return;
-    }
-    
-    var queryBar = document.getElementById("gbq2");
-    if (!queryBar)
-    {
-        console.log("Could not find the query bar.");
-        return;
-    }
-    
-    var queryBarFirstChild = queryBar.firstChild;
-    
-    var navigateBack = Nuvola.makeElement("button", null, "<");
-    navigateBack.className = "button small vertical-align";
-    navigateBack.style.float = "left";
-    navigateBack.style.marginRight = "0px";
-    navigateBack.style.borderTopRightRadius = "2px";
-    navigateBack.style.borderBottomRightRadius = "2px";
-    queryBar.insertBefore(navigateBack, queryBarFirstChild);
-    
-    var navigateForward = Nuvola.makeElement("button", null, ">");
-    navigateForward.className = "button small vertical-align";
-    navigateForward.style.float = "left";
-    navigateForward.style.marginRight = "15px";
-    navigateForward.style.borderLeft = "none";
-    navigateForward.style.borderTopLeftRadius = "2px";
-    navigateForward.style.borderLeftRightRadius = "2px";
-    queryBar.insertBefore(navigateForward, queryBarFirstChild);
-    
-    Nuvola.actions.bindButton(navigateBack, Nuvola.BrowserAction.GO_BACK);
-    Nuvola.actions.bindButton(navigateForward, Nuvola.BrowserAction.GO_FORWARD);
-}
-
-WebApp.getThumbs = function()
-{
-    var elm = document.querySelector("#player-right-wrapper .thumbs.rating-container");
-    return [elm, elm.childNodes[0], elm.childNodes[1]];
-}
-
-WebApp.getStars = function()
-{
-    return document.querySelector("#player-right-wrapper .stars.rating-container");
-}
-
-WebApp.toggleStarRating = function(enabled)
-{
-    if (enabled && this.starRatingEnabled !== true)
-    {
-        player.addExtraActions(STARS_ACTIONS);
-        this.starRatingEnabled = true;
-    }
-}
-
-WebApp.toggleThumbRating = function(enabled)
-{
-    if (enabled && this.thumbRatingEnabled !== true)
-    {
-        player.addExtraActions(THUMBS_ACTIONS);
-        this.thumbRatingEnabled = true;
     }
 }
 
 WebApp._luckyMix = function()
 {
-    return location.hash === "#/now" ? document.querySelector("div.ifl-group div.card[data-type=imfl] div.radio-icon") || false : false;
+    return location.hash === "#/now" ? document.querySelector("div[data-type=imfl]") || false : false;
 }
 
 WebApp.start();
